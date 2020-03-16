b112394f2a711b44d01ba762690d1a579b1b0864
Andrew Berry <andyb@deontik.com>
2020-03-16 16:19:50 +1100

    Added some additional rule samples

---
+++ rules/a_greaterthan_two.rb
@@ -0,0 +1,25 @@
+require 'java'
+require 'eventswarm-jar'
+require 'rule'
+
+java_import 'com.eventswarm.events.JsonEvent'
+java_import 'com.eventswarm.expressions.NumericValueExpression'
+java_import 'com.eventswarm.expressions.ConstantValue'
+
+#
+# Sample rule creator that matches JSON with a top level attribute `a` having a value greater than 2
+#
+# Note that Java is an obtrusively verbose language, and static type safety forces us to use various wrappers around values
+#
+# Use this as a template for other rule creators. Use of a 
+# module here helps us to avoid malicious user input (we will
+# only create instances of classes in this module)
+#
+module Rules
+  class AGreaterthanTwo
+    def create
+      expr = NumericValueExpression.new(NumericValueExpression::Comparator::GREATER, ConstantValue.new(2), JsonEvent::LongRetriever.new('a'))
+      Rule.new(expr, expr)
+    end
+  end
+end
---
+++ rules/a_is_one.rb
@@ -0,0 +1,26 @@
+require 'java'
+require 'eventswarm-jar'
+require 'rule'
+
+java_import 'com.eventswarm.events.JsonEvent'
+java_import 'com.eventswarm.expressions.ValueEqualsMatcher'
+java_import 'com.eventswarm.expressions.EventMatcherExpression'
+java_import 'com.eventswarm.expressions.ConstantValue'
+
+#
+# Sample rule creator that matches JSON with a top level attribute `a` having value 1
+#
+# Note that Java is an obtrusively verbose language, and static type safety forces us to use various wrappers around values
+#
+# Use this as a template for other rule creators. Use of a 
+# module here helps us to avoid malicious user input (we will
+# only create instances of classes in this module)
+#
+module Rules
+  class AIsOne
+    def create
+      expr = EventMatcherExpression.new(ValueEqualsMatcher.new(ConstantValue.new(1), JsonEvent::LongRetriever.new('a')))
+      Rule.new(expr, expr)
+    end
+  end
+end
